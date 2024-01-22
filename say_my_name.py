6763e058a11cd114c7df88f291aff57fea98e85b
Jeanette <khojeanette@gmail.com>
2024-01-22 13:18:30 +0000

    Initial Python file created.

---
+++ say_my_name.py
@@ -0,0 +1,13 @@
+first_name = 'Jeanette'
+# Created a variable called first_name with the string value of 'Jeanette'
+
+middle_name = 'Alfon'
+# Created a variable called middle_name with the string value of 'Alfon'
+
+last_name = 'Kho'
+# Created a variable called last_name with the string value of 'Kho'
+
+print(first_name, middle_name, last_name)
+# Concatenating the strings in the variables, first_name, middle_name and last_name
+# And then, printing those stringss
+
